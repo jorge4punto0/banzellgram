cea22d7545574820dd1cfaae98c1a370fc1b7e41
Jorgelisapa <jorgelisapa@gmail.com>
2020-06-18 18:21:20 -0500

    Creating base component structure

---
+++ src/components/Main.svelte
@@ -0,0 +1,5 @@
+<div class="Main">
+  <div class="Main-Container">
+    <slot />
+  </div>
+</div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,10 +1,21 @@
-<div class="main">
-    <h1>Hello Banzellgram</h1>
+<script>
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+</script>
+
+<div>
+  <Header />
+  <main>
+    <TimeLine />
+    <Sidebar />
+  </main>
 </div>
 
 <style>
-    :global(body) {
-        background-color: #676778;
-        color: #fff;
-    }
+  :global(body) {
+    background-color: #676778;
+    color: #fff;
+  }
 </style>
