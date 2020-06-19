1341520106b9479ec29260a62aa579a3d17ec494
Jorgelisapa <jorgelisapa@gmail.com>
2020-06-18 22:08:52 -0500

    Profile styles

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -9,3 +9,28 @@
     </div>
   </div>
 </div>
+
+<style>
+  .Profile__content {
+    display: flex;
+    align-items: center;
+  }
+  .Profile__userImg img {
+    width: 42px;
+    height: 42px;
+    border-radius: 50%;
+  }
+  .Profile__info {
+    margin: 0 0 0 0.5em;
+  }
+  .Profile__info h2 {
+    font-size: 14px;
+    color: black;
+    margin: 0;
+    padding: 0;
+  }
+  .Profile__info span {
+    font-size: 12px;
+    font-weight: normal;
+  }
+</style>
